410c2cac225bb13fb82dbda4681162b5e5bc5497
andriy-lanyk <LAM1988@ukr.net>
2021-07-09 03:33:41 +0300

    Fixed bag

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 $(document).ready(() => {
-    $('.logo-link, .menu-link').click(function(){
-        // console.log('click works');
-        const ID = $(this).attr('href');
-        // console.log(ID);
-        const OFFSET = $(ID).offset().top;
-        // console.log(OFFSET);
-        $('html').animate({
-            scrollTop: OFFSET
-        }, 1000);
-    });
+  $(".menu-link").click(function () {
+    // console.log('click works');
+    const ID = $(this).attr("href");
+    // console.log(ID);
+    const OFFSET = $(ID).offset().top;
+    // console.log(OFFSET);
+    $("html").animate(
+      {
+        scrollTop: OFFSET,
+      },
+      1000
+    );
+  });
 });
