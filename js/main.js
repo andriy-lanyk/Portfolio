0f6fb6cac2f47bea95e302c1d01f8847a9fa83c8
andriy-lanyk <LAM1988@ukr.net>
2021-07-09 04:39:19 +0300

    Змінив описання робіт

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-  $(".menu-link").click(function () {
+  $(".logo-link .menu-link").click(function () {
     // console.log('click works');
     const ID = $(this).attr("href");
     // console.log(ID);
